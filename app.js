4b911cfe8231c80840db102ff282ce7ea0f6ee7c
tthanhvu002 <81304749+tthanhvu002@users.noreply.github.com>
2022-09-02 21:47:55 +0700

    fix

---
+++ app.js
@@ -0,0 +1,40 @@
+const menus = document.querySelectorAll(".menu > li");
+
+Array.from(menus).map((item) => {
+  item.onclick = () => {
+    item.querySelector(".sub-menu").classList.toggle("active");
+    Array.from(menus).map((i) => {
+      if (!i.isSameNode(item)) {
+        i.classList.add("opacity-0");
+      }
+    });
+  };
+  window.addEventListener("click", function (e) {
+    if (item.contains(e.target)) {
+      // Clicked in box
+    } else {
+      // Clicked outside the box
+
+      Array.from(menus).map(i => {
+          i.classList.remove("opacity-0")
+        
+      })
+      item.querySelector(".sub-menu").classList.remove("active");
+    }
+  });
+});
+
+const navMbBtn = document.querySelectorAll(".nav-mobile-btn");
+const header = document.querySelectorAll(".header");
+const close = document.querySelectorAll(".close-btn");
+console.log(navMbBtn);
+Array.from(navMbBtn).map((item, index) => {
+  item.onclick = () => {
+    header[index].classList.add("active");
+  };
+});
+Array.from(close).map((item, index) => {
+  item.onclick = () => {
+    header[index].classList.add("active");
+  };
+});
